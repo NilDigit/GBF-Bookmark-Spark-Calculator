75b24d3aae19aff149c1043c22fde8bcba7a6fbc
NilDigit <nildigit@gmail.com>
2019-04-11 15:19:20 +0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# GBF-Bookmark-Spark-Calculator
+# GBF Bookmark Spark Calculator
 Web browser bookmark spark calculator for Granblue Fantasy
+
+How to use
+
+ 1. Copy content in `script.min.js`
+ 2. Create web browser bookmark with copied content
+ 3. Open `Granblue Fantasy` home page or profile page
+ 4. Click on created bookmark. That will show number of draw and spark percentage
+
+**Tested this will be safe to use. But developer will not take any responsibility. From usage results.**
---
+++ script.js
@@ -0,0 +1,30 @@
+javascript: (() => {
+    $.ajax({
+        url: "/item/gacha_ticket_and_others_list_by_filter_mode",
+        type: 'GET',
+        success: (res) => {
+            const crystal = parseInt($(".prt-stone").text());
+            var premium10PartTicket = 0;
+            var premiumDrawTicket = 0;
+
+            res[0].forEach(element => {
+                switch (element.id) {
+                    case 20010:
+                        premium10PartTicket = parseInt(element.number);
+                        break;
+
+                    case 20011:
+                        premiumDrawTicket = parseInt(element.number);
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+            const spark = 300;
+            const totalCrystalDraw = crystal / spark;
+            const totalDraw = totalCrystalDraw + (premium10PartTicket * 10) + premiumDrawTicket;
+            alert('Total draw: ' + parseInt(totalDraw) + ' / ' + spark + '\nComplete: ' + parseInt((totalDraw / spark) * 100) + '%');
+        }
+    });
+})();
